715d9d333781b6e36bdba6cee8412389df64615b
spuuun <matthew.douglas.ross@gmail.com>
2020-05-26 23:59:25 -0400

    add first useState hook to loginform-refactor

--- src/components/auth/LoginForm-refactor.js
+++ src/components/auth/LoginForm-refactor.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 //import { Link, Redirect } from "react-router-dom"
 //import { Button, Form, Header, Checkbox, Grid, Divider, Container } from 'semantic-ui-react'
 import { Grid, Divider, Button } from '@material-ui/core'
@@ -6,5 +6,5 @@ import UserManager from "../../modules/UserManager"
 import './Auth.css'
 
 export default function Login(props) {
-
+  const [loginValues, setLoginValues] = useState({})
 }
