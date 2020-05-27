9c8b1d77297fc21108fc36bdfa33e3a6b19db76c
spuuun <matthew.douglas.ross@gmail.com>
2020-05-26 23:54:11 -0400

    create refactor file for login. npm update, audit fix

---
+++ src/components/auth/LoginForm-refactor.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react"
+//import { Link, Redirect } from "react-router-dom"
+//import { Button, Form, Header, Checkbox, Grid, Divider, Container } from 'semantic-ui-react'
+import { Grid, Divider, Button } from '@material-ui/core'
+import UserManager from "../../modules/UserManager"
+import './Auth.css'
+
+export default function Login(props) {
+
+}
